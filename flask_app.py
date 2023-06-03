4d2338b7abdf6baddf84a0ae99314c18799bddc7
bokkaebi <h2010073@nushigh.edu.sg>
2023-06-03 23:26:58 +0800

    flasking

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,29 @@
 from flask import Flask, request, render_template, render_template_string
 from pathlib import Path
-import tensorflow as tf
+# import tensorflow as tf
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+from sentence_transformers import SentenceTransformer, util
+from sklearn.metrics.pairwise import cosine_similarity
 
 CWD = Path(__file__).parent.resolve()
 
 app = Flask(__name__)
+
+data = pd.read_csv("./data/goodreads_data.csv").drop("Unnamed: 0", axis=1)
+data = data.dropna()
+data = data.drop_duplicates()
+embeddings = np.load('data/embeddings.npy')
+def get_recommendations(book_id, similarity_matrix, k=5):
+    similar_books = list(enumerate(similarity_matrix[int(book_id)]))
+    similar_books = sorted(similar_books, key=lambda x: x[1], reverse=True)
+    similar_books = similar_books[1:k+1]
+    recommended_books = [{"title": data.iloc[i[0]]["Book"], "author": data.iloc[i[0]]["Author"]} for i in similar_books]
+    return recommended_books
+cos_sim = util.cos_sim(embeddings, embeddings)
+
 
 @app.route('/', methods=['GET'])
 def main():
@@ -14,6 +33,15 @@ def main():
 def about():
     return render_template('about.html')
 
+@app.route('/recommend', methods=['GET'])
+def recomment():
+    bookid = request.args.get("book_id")
+
+    results = get_recommendations(int(bookid), cos_sim)
+
+    return {
+        "recommendations": results
+    }
 
 
 app.run(debug=True, port=5000)
---
+++ static/bot.js
@@ -0,0 +1,93 @@
+document.getElementById("input-form").addEventListener("submit", function(event) {
+  	event.preventDefault()
+
+    
+    const cardContainer = document.getElementById('div-recommedations');
+
+	  var bookidValue = document.getElementById("input-title").value
+
+    getRecommendations(bookidValue)
+});
+
+async function getRecommendations(book_id) {
+  
+  const cardContainer = document.getElementById('div-recommedations');
+  const response = await (await fetch("recommend?book_id="+book_id)).json()
+
+  recommedations = response['recommendations']
+  console.log(recommedations)
+
+    for (const book of recommedations) {
+      const card = createCard(book['title'], book['author']);
+      cardContainer.appendChild(card);
+    }
+}
+
+
+
+
+// Function to create a card element
+function createCard(title, author) {
+    const card = document.createElement('div');
+    card.className = "recommendation-result-card"
+    card.classList.add('card');
+    
+    // const cardImage = document.createElement('img');
+    // cardImage.src = image;
+    // cardImage.alt = title;
+    // card.appendChild(cardImage);
+    
+    const cardContent = document.createElement('div');
+    cardContent.classList.add('card-content');
+    
+    const cardTitle = document.createElement('h2');
+    cardTitle.textContent = title;
+    cardContent.appendChild(cardTitle);
+    
+    const cardAuthor = document.createElement('p');
+    cardAuthor.textContent = `Author: ${author}`;
+    cardContent.appendChild(cardAuthor);
+    
+    // const cardSimilarity = document.createElement('p');
+    // cardSimilarity.textContent = `Similarity: ${similarity*100}%`;
+    // cardContent.appendChild(cardSimilarity);
+    
+    card.appendChild(cardContent);
+    return card;
+  }
+
+
+
+
+// async function getGoodreadsBook(link) {
+//     try {
+//         // Fetch the HTML content of the Goodreads book page
+//         const response = await fetch(link);
+//         const html = await response.text();
+    
+//         // Create a temporary element to parse the HTML content
+//         const parser = new DOMParser();
+//         const doc = parser.parseFromString(html, 'text/html');
+    
+//         // Extract the book details
+//         const titleElement = doc.querySelector('.Text Text__title1');
+//         const title = titleElement.textContent.trim();
+    
+//         const authorElement = doc.querySelector('ContributorLink__name')[0];
+//         const author = authorElement.textContent.trim();
+    
+//         const imageElement = doc.querySelector('.BookCover img');
+//         const imageUrl = imageElement.src;
+    
+//         // Return the book details
+//         return {
+//           title,
+//           author,
+//           imageUrl
+//         };
+//       } 
+//       catch (error) {
+//         console.error('Error:', error);
+//         throw new Error('Failed to retrieve book details.');
+//       }
+// }
